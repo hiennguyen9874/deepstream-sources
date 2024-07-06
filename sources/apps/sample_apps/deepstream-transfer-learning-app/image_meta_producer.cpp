eed395f8c09972e74bbf8b18d0b4cdba00a8c172
Xuan Hien <hiennguyen9874@gmail.com>
2024-07-06 15:56:45 +0700

    remove copyright

--- sources/apps/sample_apps/deepstream-transfer-learning-app/image_meta_producer.cpp
+++ sources/apps/sample_apps/deepstream-transfer-learning-app/image_meta_producer.cpp
@@ -1,25 +1,3 @@
-/*
- * Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-
 #include "image_meta_producer.h"
 
 typedef std::pair<std::string, std::string> string_pair;
@@ -167,23 +145,17 @@ std::string ImageMetaProducer::make_kitti_data(const IPData &data)
     // Please refer to :
     // https://docs.nvidia.com/tao/tao-toolkit/text/data_annotation_format.html#object-detection-kitti-format
     ss << data.class_name << " "; // Class names
-    ss << "0.0"
-       << " "; // Truncation (No data default value)
-    ss << "3"
-       << " "; // Occlusion [ 0 = fully visible, 1 = partly visible, 2 = largely occluded, 3 =
-               // unknown].
-    ss << "0.0"
-       << " "; // Alpha (No data default value)
+    ss << "0.0" << " ";           // Truncation (No data default value)
+    ss << "3" << " "; // Occlusion [ 0 = fully visible, 1 = partly visible, 2 = largely occluded, 3
+                      // = unknown].
+    ss << "0.0" << " "; // Alpha (No data default value)
     // Bounding box coordinates:
     ss << data.img_left << " ";                    // ymin
     ss << data.img_top << " ";                     // xmin
     ss << (data.img_left + data.img_width) << " "; // ymax
     ss << (data.img_top + data.img_height) << " "; // xmax
-    ss << "0.0 0.0 0.0"
-       << " "; // 3-D dimension (No data default value)
-    ss << "0.0 0.0 0.0"
-       << " "; // Location (No data default value)
-    ss << "0.0"
-       << " "; // Rotation_y (No data default value)
+    ss << "0.0 0.0 0.0" << " ";                    // 3-D dimension (No data default value)
+    ss << "0.0 0.0 0.0" << " ";                    // Location (No data default value)
+    ss << "0.0" << " ";                            // Rotation_y (No data default value)
     return ss.str();
 }
