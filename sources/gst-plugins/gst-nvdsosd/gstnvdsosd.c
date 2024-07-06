740e314cb9a904401ac8ddc6ca4aec534636ee17
Xuan Hien <hiennguyen9874@gmail.com>
2024-07-06 15:55:58 +0700

    remove copyright

--- sources/gst-plugins/gst-nvdsosd/gstnvdsosd.c
+++ sources/gst-plugins/gst-nvdsosd/gstnvdsosd.c
@@ -1,15 +1,3 @@
-/**
- * Copyright (c) 2016-2020, NVIDIA CORPORATION.  All rights reserved.
- *
- * NVIDIA Corporation and its licensors retain all intellectual property
- * and proprietary rights in and to this software, related documentation
- * and any modifications thereto.  Any use, reproduction, disclosure or
- * distribution of this software and related documentation without an express
- * license agreement from NVIDIA Corporation is strictly prohibited.
- *
- * version: 0.1
- */
-
 #include "gstnvdsosd.h"
 
 #include <cuda.h>
--- sources/libs/nvmsgconv/nvmsgconv.cpp
+++ sources/libs/nvmsgconv/nvmsgconv.cpp
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2018-2020, NVIDIA CORPORATION.  All rights reserved.
- *
- * NVIDIA Corporation and its licensors retain all intellectual property
- * and proprietary rights in and to this software, related documentation
- * and any modifications thereto.  Any use, reproduction, disclosure or
- * distribution of this software and related documentation without an express
- * license agreement from NVIDIA Corporation is strictly prohibited.
- *
- */
-
 #include "nvmsgconv.h"
 
 #include <json-glib/json-glib.h>
@@ -829,8 +818,8 @@ static gchar *generate_deepstream_message_minimal(NvDsMsg2pCtx *ctx, NvDsEvent *
                     ss << "|#|" << to_str(dsObj->gender) << "|" << dsObj->age << "|"
                        << to_str(dsObj->hair) << "|" << to_str(dsObj->cap) << "|"
                        << to_str(dsObj->glasses) << "|" << to_str(dsObj->facialhair) << "|"
-                       << to_str(dsObj->name) << "|"
-                       << "|" << to_str(dsObj->eyecolor) << "|" << meta->confidence;
+                       << to_str(dsObj->name) << "|" << "|" << to_str(dsObj->eyecolor) << "|"
+                       << meta->confidence;
                 }
             } break;
             case NVDS_OBJECT_TYPE_VEHICLE_EXT: {
@@ -862,8 +851,8 @@ static gchar *generate_deepstream_message_minimal(NvDsMsg2pCtx *ctx, NvDsEvent *
                     ss << "|#|" << to_str(dsObj->gender) << "|" << dsObj->age << "|"
                        << to_str(dsObj->hair) << "|" << to_str(dsObj->cap) << "|"
                        << to_str(dsObj->glasses) << "|" << to_str(dsObj->facialhair) << "|"
-                       << to_str(dsObj->name) << "|"
-                       << "|" << to_str(dsObj->eyecolor) << "|" << meta->confidence;
+                       << to_str(dsObj->name) << "|" << "|" << to_str(dsObj->eyecolor) << "|"
+                       << meta->confidence;
 
                     if (dsObj->mask)
                         objectMask = dsObj->mask;
